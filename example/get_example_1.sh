ff95d9cba215e75c6e4ac91cf3bfad083cd9e2ea
Yoichi Hirai <i@yoichihirai.com>
2016-09-27 18:05:39 +0200

    Add the first example to enrich

---
+++ example/get_example_1.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+# Run a geth node and sync it up to around block 1,000,000.
+# Then run this command to get an example vm trace.
+
+geth --exec 'debug.traceTransaction("f682a82e7e1d1410f4f3ff208746dc0cd447a14a0afc575976e9949499175fc5")' attach
