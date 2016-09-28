731a42eb4d8e8426c5e45dd91115b0f1da0471ed
Yoichi Hirai <i@yoichihirai.com>
2016-09-28 10:10:30 +0200

    Fix the JSON printing
    Previously `get_example_1.sh` was not printing a JSON but just showing
    a javascript value in a javascript console.

--- example/get_example_1.sh
+++ example/get_example_1.sh
@@ -3,4 +3,4 @@
 # Run a geth node and sync it up to around block 1,000,000.
 # Then run this command to get an example vm trace.
 
-geth --exec 'debug.traceTransaction("f682a82e7e1d1410f4f3ff208746dc0cd447a14a0afc575976e9949499175fc5")' attach
+geth --exec 'console.log(JSON.stringify(debug.traceTransaction("f682a82e7e1d1410f4f3ff208746dc0cd447a14a0afc575976e9949499175fc5")))' attach | head -n 1
